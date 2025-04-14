e261bdf2541c8e1070e1b71fa50acb06abcf81fa
Christophe Dufour <blackfox@proximus.be>
2025-04-14 23:58:38 +0200

    Modification de la classe Car

--- src/Lib/Entities/Car.php
+++ src/Lib/Entities/Car.php
@@ -4,6 +4,7 @@
  * Car.php
  * @Auteur : Christophe Dufour
  * 
+ * Classe modélisant une voiture
  */
 
  namespace Lib\Entities;
@@ -23,14 +24,18 @@
 
     // Modèle d'une voiture
     protected string $model;
+    // Est-ce une voiture favorite
+    protected bool $favorite = false;
 
     /**
      * Getters
-     * *******
      */
 
     /**
      * Retourne la valeur de l'attribut $model
+     * 
+     * @return string
+     * Retourne le model de voiture
      */
     public function model(): string
     {
@@ -38,12 +43,27 @@
     }
 
     /**
+     * Retourne la valeur de l'attribut $favorite
+     * 
+     * @return bool
+     * Retourne true si la voiture est dans les favorites, sinon false
+     */
+    public function favorite(): bool
+    {
+        return $this->favorite;
+    }
+
+    /**
      * Setters
-     * *******
      */
 
     /**
      * Modifie la valeur de l'attribut $model
+     * 
+     * @param string $model
+     * Modèle d'une voiture
+     * @return void
+     * Ne retourne aucune valeur
      */
     public function setModel(string $model): void
     {
@@ -54,20 +74,38 @@
     }
 
     /**
+     * Modifie la valeur de l'attribut $favorite
+     * 
+     * @param bool $favorite
+     * Un booléen, true si la voiture dans la liste des favorites, sinon false
+     * @return void
+     * Ne retourne aucune valeur
+     */
+    public function setFavorite(bool $favorite): void
+    {
+        $this->favorite = $favorite;
+    }
+
+    /**
      * Methodes
-     * ********
      */
 
     /**
-     * Retourne true si l'objet est valide
+     * Vérifie si un object est valide
+     * 
+     * @return bool
+     * Retourne true en cas de succès, sinon false
      */
 	public function isValid(): bool
 	{
-		return !$this->hasErrors() && !empty($this->model);
+		return !$this->hasErrors() && !empty($this->model) && is_bool($this->favorite);
     }
 
     /**
      * Retourne l'objet sour forme de chaine de caratère
+     * 
+     * @return string
+     * Retourne le model de voiture
      */
 	public function __toString(): string
 	{
