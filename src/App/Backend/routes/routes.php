a1d172cbd5fc0f1607a3961164c145b021dded78
Christophe Dufour <blackfox@proximus.be>
2025-04-20 12:46:57 +0200

    Ajouts de routes et de liens

--- src/App/Backend/routes/routes.php
+++ src/App/Backend/routes/routes.php
@@ -15,10 +15,15 @@ Router::set("/admin/", "Acc", "consumptions");
 
 Router::set("/admin/cars", "Acc", "cars");
 Router::set("/admin/addCar", "Acc", "addCar");
+Router::set("/admin/editCar-([0-9a-f]{64})-([0-9]+)", "Acc", "editCar", "security,idCar");
+Router::set("/admin/modifyCar-([0-9a-f]{64})-([0-9]+)", "Acc", "modifyCar", "security,idCar");
 Router::set("/admin/favoriteCar-([0-9a-f]{64})-([0-9]+)", "Acc", "favoriteCar", "security,idCar");
 Router::set("/admin/deleteCar-([0-9a-f]{64})-([0-9]+)", "Acc", "deleteCar", "security,idCar");
 
 Router::set("/admin/circuits", "Acc", "circuits");
+Router::set("/admin/addCircuit", "Acc", "addCircuit");
+Router::set("/admin/editCircuit-([0-9a-f]{64})-([0-9]+)", "Acc", "editCircuit", "security,idCircuit");
+Router::set("/admin/modifyCircuit-([0-9a-f]{64})-([0-9]+)", "Acc", "modifyCircuit", "security,idCircuit");
 Router::set("/admin/deleteCircuit-([0-9a-f]{64})-([0-9]+)", "Acc", "deleteCircuit", "security,idCircuit");
 
 Router::set("/admin/consumptions", "Acc", "consumptions");
